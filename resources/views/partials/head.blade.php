aeff22b50e17fad3f9b7699adf7532b0fcc27b25
creativehabib <shamolrahaman@gmail.com>
2026-06-24 22:25:50 +0600

    update

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,7 +8,9 @@
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@400;500;600;700&display=swap" rel="stylesheet">
 @fonts
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
