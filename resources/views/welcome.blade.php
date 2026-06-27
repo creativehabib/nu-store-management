6cb68341eb7b5fe9236c6f293b468d2e55226d2f
creativehabib <shamolrahaman@gmail.com>
2026-06-27 19:17:51 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ setting('site_name', 'Store Management System') }} | {{ __('National University') }}</title>
-    <link rel="icon" href="{{ asset(setting('site_favicon')) }}" sizes="any">
+    <link rel="icon" href="/logo.png" sizes="any">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
@@ -32,7 +32,7 @@
     <div class=" border-b border-zinc-200 dark:border-zinc-800">
         <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
             <div class="flex items-center gap-3">
-                <a href="/"><img src="{{ asset(setting('site_logo') ?? asset('logo.png')) }}" class="w-8 h-10" alt="Logo"></a>
+                <a href="/"><img src="{{  asset('logo.png') }}" class="w-8 h-10" alt="Logo"></a>
                 <div>
                     <h2 class="font-bold text-lg leading-tight">National University</h2>
                     <span class="text-xs text-zinc-500 uppercase tracking-widest">Bangladesh</span>
